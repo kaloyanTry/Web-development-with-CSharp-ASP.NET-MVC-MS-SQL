7068668d96203160d40272f312a073a358af7631
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-24 21:56:35 +0300

    Add files via upload

---
+++ FullStackCourse/PersonalTrackingApp/BLL/EmployeeBLL.cs
@@ -0,0 +1,23 @@
+﻿using DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DAO;
+
+
+namespace BLL
+{
+    public class EmployeeBLL
+    {
+        public static EmployeeDTO GetAll()
+        {
+            EmployeeDTO dto = new EmployeeDTO();
+            dto.Departments = DepartmentDAO.GetDepartments();
+            dto.Positions = PositionDAO.GetPositions();
+
+            return dto;
+        }
+    }
+}
