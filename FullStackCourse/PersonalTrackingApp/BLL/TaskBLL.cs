2bf0ac34c3013a8d9d4f3326c0b93478f08b8f16
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-30 22:05:53 +0300

    Add files via upload

---
+++ FullStackCourse/PersonalTrackingApp/BLL/TaskBLL.cs
@@ -0,0 +1,46 @@
+﻿using DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DAO;
+using DAL;
+using Task = DAL.Task; //adding for the mistake
+
+namespace BLL
+{
+    public class TaskBLL
+    {
+        public static TaskDTO GetAll()
+        {
+            TaskDTO taskdto = new TaskDTO();
+            taskdto.Employees = EmployeeDAO.GetEmployees();
+            taskdto.Departments = DepartmentDAO.GetDepartments();
+            taskdto.Positions = PositionDAO.GetPositions();
+            taskdto.TaskStates = TaskDAO.GetTaskState();
+            taskdto.Tasks = TaskDAO.GetTasks();
+            return taskdto;
+        }
+
+        public static void AddTask(Task task)
+        {
+            TaskDAO.AddTask(task);
+        }
+
+        //public static void UpdateTask(TASK task)
+        //{
+        //    TaskDAO.UpdateTask(task);
+        //}
+
+        //public static void DeleteTask(int taskID)
+        //{
+        //    TaskDAO.DeleteTask(taskID);
+        //}
+
+        //public static void ApproveTask(int taskID, bool isAdmin)
+        //{
+        //    TaskDAO.ApproveTask(taskID, isAdmin);
+        //}
+    }
+}
