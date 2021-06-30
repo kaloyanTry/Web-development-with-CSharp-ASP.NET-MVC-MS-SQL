1e228bb4117c92906225cadeac4ad9b92efe5239
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-30 22:07:56 +0300

    Add files via upload

---
+++ FullStackCourse/PersonalTrackingApp/DAL/DAO/TaskDAO.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DTO;
+
+namespace DAL.DAO
+{
+    public class TaskDAO : EmployeeContext
+    {
+        public static List<TaskState> GetTaskState()
+        {
+            return db.TaskStates.ToList();
+        }
+
+        public static void AddTask(Task task)
+        {
+            try
+            {
+                db.Tasks.InsertOnSubmit(task);
+                db.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static List<TaskDetailDTO> GetTasks()
+        {
+            List<TaskDetailDTO> tasklist = new List<TaskDetailDTO>();
+
+            var list = (from t in db.Tasks
+                        join s in db.TaskStates on t.TaskState equals s.ID
+                        join e in db.Employees on t.EmployeeID equals e.ID
+                        join d in db.Departments on e.DepartmentID equals d.ID
+                        join p in db.Positions on e.PositionID equals p.ID
+                        select new
+                        {
+                            taskID = t.ID,
+                            title = t.TaskTitle,
+                            content = t.TaskContent,
+                            startdate = t.TaskStartDate,
+                            deliveryDate = t.TaskDeliveryDate,
+                            taskStateName = s.StateName,
+                            taskStateID = t.TaskState,
+                            UserNo = e.UserNumber,
+                            Name = e.Name,
+                            EmployeeId = t.EmployeeID,
+                            Surname = e.Surname,
+                            positionName = p.PositionName,
+                            departmanName = d.DepartmentName,
+                            positionID = e.PositionID,
+                            departmentId = e.DepartmentID
+                        }).OrderBy(x => x.startdate).ToList();
+
+            foreach (var item in list)
+            {
+                TaskDetailDTO dto = new TaskDetailDTO();
+                dto.TaskID = item.taskID;
+                dto.Title = item.title;
+                dto.Content = item.content;
+                dto.TaskStartDate = item.startdate;
+                dto.TaskDeliveryDate = item.deliveryDate;
+                dto.TaskStateName = item.taskStateName;
+                dto.taskStateID = item.taskStateID;
+                dto.UserNo = item.UserNo;
+                dto.Name = item.Name;
+                dto.Surname = item.Surname;
+                dto.DepartmentName = item.departmanName;
+                dto.PositionID = item.positionID;
+                dto.PositionName = item.positionName;
+                dto.EmployeeID = item.EmployeeId;
+                tasklist.Add(dto);
+                
+            }
+            return tasklist;
+        }
+    }
+}
