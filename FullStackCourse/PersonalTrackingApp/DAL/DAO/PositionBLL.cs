7f4d8ff849e6a3d05f421d5d7f61d18f97e9d6b4
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-07-02 15:44:19 +0300

    Add files via upload

---
+++ FullStackCourse/PersonalTrackingApp/DAL/DAO/PositionBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using DAL.DAO;
+using DAL.DTO;
+
+namespace BLL
+{
+    public class PositionBLL
+    {
+        public static void AddPosition(Position position)
+        {
+            PositionDAO.AddPosition(position);
+        }
+
+        public static List<PositionDTO> GetPositions()
+        {
+            return PositionDAO.GetPositions();
+        }
+
+        public static void UpdatePosition(Position position, bool control)
+        {
+            //PositionDAO.UpdatePosition(position);
+            //if (control)
+            //{
+            //    EmployeeDAO.UpdateEmployee(position);
+            //}       
+        }
+
+        public static void DeletePosition(int iD)
+        {
+            //PositionDAO.DeletePosition(iD);
+        }
+    }
+}
