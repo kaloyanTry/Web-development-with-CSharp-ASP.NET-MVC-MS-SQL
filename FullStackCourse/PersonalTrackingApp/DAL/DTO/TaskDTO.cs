a083d796798782f5f7a0af1d2873991da4f9411e
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-07-03 07:28:38 +0300

    Add files via upload

---
+++ FullStackCourse/PersonalTrackingApp/DAL/DTO/TaskDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DTO
+{
+    public class TaskDTO
+    {
+        public List<EmployeeDetailDTO> Employees { get; set; }
+        public List<Department> Departments { get; set; }
+        public List<PositionDTO> Positions { get; set; }
+        public List<TaskState> TaskStates { get; set; }
+        public List<TaskDetailDTO> Tasks { get; set; }
+    }
+}
