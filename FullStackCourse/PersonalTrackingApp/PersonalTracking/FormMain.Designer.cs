bf0825ecb8a889af3566a1e7de0da0a4430a95b2
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-07-05 16:41:27 +0300

    Add files via upload

--- FullStackCourse/PersonalTrackingApp/PersonalTracking/FormMain.Designer.cs
+++ FullStackCourse/PersonalTrackingApp/PersonalTracking/FormMain.Designer.cs
@@ -42,17 +42,18 @@ namespace PersonalTracking
             // 
             // btnEmployee
             // 
-            this.btnEmployee.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnEmployee.BackColor = System.Drawing.Color.Bisque;
             this.btnEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnEmployee.Image = ((System.Drawing.Image)(resources.GetObject("btnEmployee.Image")));
             this.btnEmployee.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.btnEmployee.Location = new System.Drawing.Point(12, 12);
             this.btnEmployee.Name = "btnEmployee";
-            this.btnEmployee.Size = new System.Drawing.Size(138, 109);
+            this.btnEmployee.Size = new System.Drawing.Size(172, 124);
             this.btnEmployee.TabIndex = 0;
             this.btnEmployee.Text = "Employee";
             this.btnEmployee.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnEmployee.UseVisualStyleBackColor = false;
+            this.btnEmployee.Click += new System.EventHandler(this.btnEmployee_Click);
             // 
             // btnTasks
             // 
@@ -60,93 +61,99 @@ namespace PersonalTracking
             this.btnTasks.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnTasks.Image = ((System.Drawing.Image)(resources.GetObject("btnTasks.Image")));
             this.btnTasks.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnTasks.Location = new System.Drawing.Point(156, 12);
+            this.btnTasks.Location = new System.Drawing.Point(190, 12);
             this.btnTasks.Name = "btnTasks";
-            this.btnTasks.Size = new System.Drawing.Size(138, 109);
+            this.btnTasks.Size = new System.Drawing.Size(172, 124);
             this.btnTasks.TabIndex = 1;
             this.btnTasks.Text = "Tasks";
             this.btnTasks.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnTasks.UseVisualStyleBackColor = false;
+            this.btnTasks.Click += new System.EventHandler(this.btnTasks_Click);
             // 
             // btnSalary
             // 
-            this.btnSalary.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnSalary.BackColor = System.Drawing.Color.YellowGreen;
             this.btnSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalary.Image = ((System.Drawing.Image)(resources.GetObject("btnSalary.Image")));
             this.btnSalary.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnSalary.Location = new System.Drawing.Point(300, 12);
+            this.btnSalary.Location = new System.Drawing.Point(368, 12);
             this.btnSalary.Name = "btnSalary";
-            this.btnSalary.Size = new System.Drawing.Size(138, 109);
+            this.btnSalary.Size = new System.Drawing.Size(172, 124);
             this.btnSalary.TabIndex = 2;
             this.btnSalary.Text = "Salary";
             this.btnSalary.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnSalary.UseVisualStyleBackColor = false;
+            this.btnSalary.Click += new System.EventHandler(this.btnSalary_Click);
             // 
             // btnPermission
             // 
-            this.btnPermission.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnPermission.BackColor = System.Drawing.Color.LimeGreen;
             this.btnPermission.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPermission.Image = ((System.Drawing.Image)(resources.GetObject("btnPermission.Image")));
             this.btnPermission.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnPermission.Location = new System.Drawing.Point(12, 127);
+            this.btnPermission.Location = new System.Drawing.Point(12, 142);
             this.btnPermission.Name = "btnPermission";
-            this.btnPermission.Size = new System.Drawing.Size(138, 109);
+            this.btnPermission.Size = new System.Drawing.Size(172, 124);
             this.btnPermission.TabIndex = 3;
             this.btnPermission.Text = "Permission";
             this.btnPermission.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnPermission.UseVisualStyleBackColor = false;
+            this.btnPermission.Click += new System.EventHandler(this.btnPermission_Click);
             // 
             // btnDepartment
             // 
-            this.btnDepartment.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnDepartment.BackColor = System.Drawing.Color.LemonChiffon;
             this.btnDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnDepartment.Image = ((System.Drawing.Image)(resources.GetObject("btnDepartment.Image")));
             this.btnDepartment.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnDepartment.Location = new System.Drawing.Point(156, 127);
+            this.btnDepartment.Location = new System.Drawing.Point(190, 142);
             this.btnDepartment.Name = "btnDepartment";
-            this.btnDepartment.Size = new System.Drawing.Size(138, 109);
+            this.btnDepartment.Size = new System.Drawing.Size(172, 124);
             this.btnDepartment.TabIndex = 4;
             this.btnDepartment.Text = "Department";
             this.btnDepartment.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnDepartment.UseVisualStyleBackColor = false;
+            this.btnDepartment.Click += new System.EventHandler(this.btnDepartment_Click);
             // 
             // btnPosition
             // 
-            this.btnPosition.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnPosition.BackColor = System.Drawing.Color.MistyRose;
             this.btnPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPosition.Image = ((System.Drawing.Image)(resources.GetObject("btnPosition.Image")));
             this.btnPosition.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnPosition.Location = new System.Drawing.Point(300, 127);
+            this.btnPosition.Location = new System.Drawing.Point(368, 142);
             this.btnPosition.Name = "btnPosition";
-            this.btnPosition.Size = new System.Drawing.Size(138, 109);
+            this.btnPosition.Size = new System.Drawing.Size(172, 124);
             this.btnPosition.TabIndex = 5;
             this.btnPosition.Text = "Position";
             this.btnPosition.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnPosition.UseVisualStyleBackColor = false;
+            this.btnPosition.Click += new System.EventHandler(this.btnPosition_Click);
             // 
             // btnLogOut
             // 
-            this.btnLogOut.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnLogOut.BackColor = System.Drawing.Color.Gold;
             this.btnLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogOut.Image = ((System.Drawing.Image)(resources.GetObject("btnLogOut.Image")));
             this.btnLogOut.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnLogOut.Location = new System.Drawing.Point(12, 242);
+            this.btnLogOut.Location = new System.Drawing.Point(12, 272);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(138, 109);
+            this.btnLogOut.Size = new System.Drawing.Size(172, 124);
             this.btnLogOut.TabIndex = 6;
             this.btnLogOut.Text = "Log out";
             this.btnLogOut.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
             // btnExit
             // 
-            this.btnExit.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnExit.BackColor = System.Drawing.Color.OrangeRed;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
             this.btnExit.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.btnExit.Location = new System.Drawing.Point(156, 242);
+            this.btnExit.Location = new System.Drawing.Point(190, 272);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(138, 109);
+            this.btnExit.Size = new System.Drawing.Size(172, 124);
             this.btnExit.TabIndex = 7;
             this.btnExit.Text = "Exit";
             this.btnExit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -157,7 +164,7 @@ namespace PersonalTracking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(682, 453);
+            this.ClientSize = new System.Drawing.Size(782, 553);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnLogOut);
             this.Controls.Add(this.btnPosition);
@@ -169,6 +176,7 @@ namespace PersonalTracking
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Employee Tracking";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
             this.ResumeLayout(false);
 
         }
